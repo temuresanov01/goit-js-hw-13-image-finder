65b03e550654b3b8748424bd9362208e0bf12590
Temur Esanov <temuresanov01@gmail.com>
2021-10-26 22:42:05 +0200

    correct api.js_4

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
-const API_KEY = '23996905-c7bf8d03157e8f53790f8e72e'
-const BASE_URL = 'https://pixabay.com/api/'
+const API_KEY = '23996905-c7bf8d03157e8f53790f8e72e';
+// const BASE_URL = 'https://pixabay.com/api/'
 
 const fetchImages = (webSearch, webPage) => {
   const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${webSearch}&page=${webPage}&per_page=12&key=${keyPixabay}`;
