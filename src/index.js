0031ca061662e560ba87cf5bcec9211443b5a869
Temur Esanov <temuresanov01@gmail.com>
2021-10-26 21:58:15 +0200

    update index.js_2

--- src/index.js
+++ src/index.js
@@ -28,3 +28,41 @@ function loadMore(event) {
   webPage += 1;
   getPrintImages(true);
 }
+function getPrintImages(winScroll) {
+  fetchData(searchWord, page).then(data => {
+    const cards = galleryCard(data.hits);
+    const hitsLength = data.hits.length;
+
+    galleryRef.insertAdjacentHTML('beforeend', cards);
+    btnLoadMoreRef.classList.add('is-open');
+    formRef.reset();
+
+    if (hitsLength < 1) {
+      btnLoadMoreRef.classList.remove('is-open');
+    }
+      if (hitsLength === 0) {
+        alert ('Введите правильное название поиска, пожалуйста!')
+    //   alert('Enter correct name of search please!');
+    }
+    if (winScroll) {
+      winScroll();
+    }
+  });
+}
+
+function scroll() {
+  window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: 'smooth',
+  });
+}
+
+function biggestFoto(event) {
+  if (event.target.tagName !== 'IMG') return;
+
+  const largeImageURL = event.target.dataset.bigImage;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="420" height="500">
+`);
+  instance.show();
+}
