ad96ffe7589492be2c4327d8d04e977d69597661
Temur Esanov <temuresanov01@gmail.com>
2021-10-26 21:27:33 +0200

    update index.js_1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+import { fetchImages } from './js/api';
+import galleryCard from './templates/galleryCard';
+
+const formRef = document.querySelector('.search-form');
+const inputRef = document.querySelector('.search-form_input');
+const galleryRef = document.querySelector('.gallery');
+const btnLoadMoreRef = document.querySelector('.load-more');
+
+formRef.addEventListener('submit', requestForm);
+btnLoadMoreRef.addEventListener('click', loadMore);
+galleryRef.addEventListener('click', bigImage);
+
+let webSearch = '';
+let webPage = 1;
+
+function requestForm(event) {
+  event.preventDefault();
+  galleryRef.innerHTML = '';
+  webPage = 1;
+  webSearch = inputRef.value;
+  if (webSearch === '') return;
+
+  getPrintImages(false);
+}
+
+function loadMore(event) {
+  webPage += 1;
+  getPrintImages(true);
+}
