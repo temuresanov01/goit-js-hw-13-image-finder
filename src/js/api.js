fa3fd05d1947f65737f3c1633a669da3a3196e02
Temur Esanov <temuresanov01@gmail.com>
2021-10-27 06:22:48 +0200

    corrent api.js

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@ const API_KEY = '23996905-c7bf8d03157e8f53790f8e72e';
 // const BASE_URL = 'https://pixabay.com/api/'
 
 const fetchImages = (webSearch, webPage) => {
-  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${webSearch}&page=${webPage}&per_page=12&key=${keyPixabay}`;
+  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${webSearch}&page=${webPage}&per_page=12&key=${API_KEY}`;
   return fetch(BASE_URL)
     .then(response => response.json())
     .catch(err => alert(err));
